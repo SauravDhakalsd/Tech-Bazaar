6aaa92e8324e1872d7f2ed5ad9388440956836b4
Saurav Dhakal <sauravdhakal.work@gmail.com>
2022-10-03 16:09:09 +0545

    Build

---
+++ frontend/build/precache-manifest.ef6e79c8924def8c3ebd2c48bf5c2a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d21da105e8d45c2bd469ef28edbd7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b21851a811ce637970bc",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "c921133f2441b68440b5",
+    "url": "/static/js/2.414cd5bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.414cd5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21851a811ce637970bc",
+    "url": "/static/js/main.66c06c5c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
