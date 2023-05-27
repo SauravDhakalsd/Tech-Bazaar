4e8a7c500d6a1f27936bfa5d4a1a3d4f221b1e60
Saurav Dhakal <86107331+SauravDhakalsd@users.noreply.github.com>
2023-05-27 23:51:54 +0545

    Build folder for update

---
+++ frontend/build/precache-manifest.e4d10319d265a531cca8593162402a7d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed6227647a029262552718a796ffcdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f6c554862ddf3e47ad2",
+    "url": "/static/css/main.d23f1def.chunk.css"
+  },
+  {
+    "revision": "e77ae8a45e48aa391014",
+    "url": "/static/js/2.3ae339cb.chunk.js"
+  },
+  {
+    "revision": "10e35354e9b0837a436e179e3ef82e09",
+    "url": "/static/js/2.3ae339cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6c554862ddf3e47ad2",
+    "url": "/static/js/main.5ab161a7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5ab161a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "6130576e307948404dc29eb597ebbe33",
+    "url": "/static/media/close.6130576e.svg"
+  },
+  {
+    "revision": "a9fc121874cc82f5ab237d28974dd78d",
+    "url": "/static/media/track.a9fc1218.svg"
+  },
+  {
+    "revision": "113ee5e388a50a19c3f62964d84c43ec",
+    "url": "/static/media/uploadImage.113ee5e3.svg"
+  }
+]);
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e45ae3afdef94488bd1572dd9b7aee6.js"
+  "/precache-manifest.e4d10319d265a531cca8593162402a7d.js"
 );
 
 self.addEventListener('message', (event) => {
