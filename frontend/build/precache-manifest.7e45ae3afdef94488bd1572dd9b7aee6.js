40e848a63c420c4437814edcf2008482b9bb8d7f
Saurav Dhakal <86107331+SauravDhakalsd@users.noreply.github.com>
2023-05-27 22:59:43 +0545

    Added AI: Making build folder

---
+++ frontend/build/precache-manifest.7e45ae3afdef94488bd1572dd9b7aee6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ea1a3d3e5ecc01d0f710dee14f1f9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "954e213afd03b4c73003",
+    "url": "/static/css/main.d23f1def.chunk.css"
+  },
+  {
+    "revision": "657b47a8fcf6320e3a51",
+    "url": "/static/js/2.d0cfe02e.chunk.js"
+  },
+  {
+    "revision": "10e35354e9b0837a436e179e3ef82e09",
+    "url": "/static/js/2.d0cfe02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "954e213afd03b4c73003",
+    "url": "/static/js/main.9bd47902.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9bd47902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "6130576e307948404dc29eb597ebbe33",
+    "url": "/static/media/close.6130576e.svg"
+  },
+  {
+    "revision": "a9fc121874cc82f5ab237d28974dd78d",
+    "url": "/static/media/track.a9fc1218.svg"
+  },
+  {
+    "revision": "113ee5e388a50a19c3f62964d84c43ec",
+    "url": "/static/media/uploadImage.113ee5e3.svg"
+  }
+]);
